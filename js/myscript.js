ab7df1c71aae5095fdc77f5da19cdde3c1994845
Ronan C <rojocas@gmail.com>
2020-01-08 09:18:46 +0000

    Added JQuery styling

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $("h2").addClass("underline"); //underlines all <h2> elements
     $("ul").addClass("border"); //adds a border to all unordered lists
+    $("tr:odd").addClass("odd"); //adds orange background to odd rows
+    $("tr:even").addClass("even"); //adds orange background to even rows
 });
